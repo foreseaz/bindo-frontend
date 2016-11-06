c831e640fe5e89235b36f8582787e7238c0e1825
Foreseaz <foreseazhang@gmail.com>
2016-11-06 20:48:57 +0800

    more

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Container from './component/Container';
-import people_data from './assets/people.json';
+import people_data from './data/people.json';
 
 ReactDOM.render(
   <Container data={people_data} />,
